7ff90e1ec650f4f94d32627d45626ba10fe33e9a
Francis Gulotta <wizard@roborooter.com>
2018-11-16 00:24:32 -0500

    docs: cleanup the examples and docs

--- example.js
+++ example.js
@@ -1,25 +1,33 @@
-const { parallelMap, map, collect } = require('streaming-iterables')
+const { parallelMap, map, consume } = require('streaming-iterables')
 const got = require('got')
 
+// A generator to fetch all the pokemon from the pokemon api
 const pokeGenerator = async function* () {
   let offset = 0
   while(true) {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}`
     const { body: pokemon } = await got(url, { json: true })
-    if (pokemon.results.length > 0) {
-      offset += pokemon.results.length
-      for (const monster of pokemon.results) {
-        yield monster
-      }
-    } else {
+    if (pokemon.results.length === 0) {
       return
+    }
+    offset += pokemon.results.length
+    for (const monster of pokemon.results) {
+      yield monster
     }
   }
 }
 
-const loadUrl = ({ url }) => got(url, { json: true }).then(resp => resp.body)
-const loadPages = parallelMap(2, loadUrl)
-const logMonsters = map(pokemon => console.log(pokemon.name, pokemon.sprites.front_default))
+// an async download function to download the data
+const loadUrl = async ({ url }) => {
+  const { body } = await got(url, { json: true })
+  return body
+}
+// a parallelMap iterator that will load the monsters two at a time
+const loadMonsters = parallelMap(2, loadUrl)
 
-await collect(logMonsters(loadPages(pokeGenerator())))
+// a map function to log monster data
+const logMonsters = map(pokemon => console.log(`${pokemon.name} ${pokemon.sprites.front_default}`))
+
+// lets do it team!
+await consume(logMonsters(loadMonsters(pokeGenerator())))
 console.log('caught them all')
