1e628af2fb926fdf93d9623002787730787fd4ae
Francis Gulotta <wizard@roborooter.com>
2018-12-14 20:42:41 -0500

    chore: add some benchmarks (#17)
    From my laptop
    ```
    async batch x 33.01 ops/sec ±1.33% (74 runs sampled)
    async collect x 33.57 ops/sec ±1.22% (75 runs sampled)
    async consume x 33.84 ops/sec ±1.08% (76 runs sampled)
    async buffer x 29.49 ops/sec ±0.86% (68 runs sampled)
    async flatTransform x 6.94 ops/sec ±3.41% (37 runs sampled)
    async map x 14.94 ops/sec ±1.51% (69 runs sampled)
    async map sync func x 25.47 ops/sec ±1.44% (61 runs sampled)
    async parallelFlatMap x 6.25 ops/sec ±3.00% (34 runs sampled)
    async parallelMap x 15.71 ops/sec ±2.27% (71 runs sampled)
    async parallelMap sync func x 16.48 ops/sec ±1.33% (74 runs sampled)
    async tap x 14.94 ops/sec ±1.85% (69 runs sampled)
    async transform x 13.03 ops/sec ±2.42% (62 runs sampled)
    async transform sync func x 20.37 ops/sec ±3.20% (51 runs sampled)
    sync batch x 3,879 ops/sec ±1.14% (83 runs sampled)
    sync collect x 5,452 ops/sec ±1.22% (89 runs sampled)
    sync consume x 6,608 ops/sec ±1.48% (80 runs sampled)
    sync buffer x 1,560 ops/sec ±1.70% (83 runs sampled)
    ```

---
+++ benchmark/index.ts
@@ -0,0 +1,185 @@
+import { Suite } from 'benchmark'
+import {
+  batch,
+  buffer,
+  collect,
+  consume,
+  flatTransform,
+  map,
+  parallelFlatMap,
+  parallelMap,
+  pipeline,
+  tap,
+  transform,
+} from '../'
+const suite = new Suite()
+
+const asyncSetImmediate = () => {
+  return new Promise(resolve => {
+    setImmediate(resolve)
+  })
+}
+
+const numbersLimit = 10000
+
+const fastNumbers = function*() {
+  let num = 0
+  while (true) {
+    yield ++num
+    if (num > numbersLimit) {
+      return
+    }
+  }
+}
+
+const slowNumbers = async function*() {
+  let num = 0
+  while (true) {
+    await asyncSetImmediate()
+    yield ++num
+    if (num > numbersLimit) {
+      return
+    }
+  }
+}
+
+const fastStringify = i => [String(i)]
+const slowStringify = i => asyncSetImmediate().then(() => [String(i)])
+
+suite.add('async batch', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, batch(10), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async collect', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, collect)
+    deferred.resolve()
+  },
+})
+
+suite.add('async consume', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async buffer', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, buffer(10), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async flatTransform', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, flatTransform(10, slowStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async map', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, map(slowStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async map sync func', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, map(fastStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async parallelFlatMap', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, parallelFlatMap(10, slowStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async parallelMap', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, parallelMap(10, slowStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async parallelMap sync func', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, parallelMap(10, fastStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async tap', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, tap(slowStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async transform', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, transform(1, slowStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('async transform sync func', {
+  defer: true,
+  fn: async deferred => {
+    await pipeline(slowNumbers, transform(1, fastStringify), consume)
+    deferred.resolve()
+  },
+})
+
+suite.add('sync batch', {
+  fn: async () => {
+    await pipeline(fastNumbers, batch(10), consume)
+  },
+})
+
+suite.add('sync collect', {
+  fn: async () => {
+    pipeline(fastNumbers, collect)
+  },
+})
+
+suite.add('sync consume', {
+  fn: async () => {
+    pipeline(fastNumbers, consume)
+  },
+})
+
+suite.add('sync buffer', {
+  fn: async () => {
+    pipeline(fastNumbers, buffer(10), consume)
+  },
+})
+
+suite.on('cycle', event => {
+  console.log(String(event.target))
+})
+
+suite.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'))
+})
+
+suite.run({ async: true })
