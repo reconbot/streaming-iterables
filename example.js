1c05d19cc4d2170cbb18af2c8898c156a86ca891
Francis Gulotta <francis.gulotta@shopify.com>
2021-01-15 15:22:52 -0500

    fix example

--- example.js
+++ example.js
@@ -1,12 +1,12 @@
 const { buffer, flatten, pipeline, transform } = require('streaming-iterables')
-const got = require('got')
+const got = require('got@11.8.1')
 
 // A generator to fetch all the pokemon from the pokemon api
 const pokedex = async function* () {
   let offset = 0
   while(true) {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}`
-    const { body: { results: pokemon } } = await got(url, { json: true })
+    const { results: pokemon } = await got(url).json()
     if (pokemon.length === 0) {
       return
     }
@@ -19,12 +19,9 @@ const pokedex = async function* () {
 const bufferTwo = buffer(2)
 
 // a transform iterator that will load the monsters two at a time and yield them as soon as they're ready
-const pokeLoader = transform(2, async ({ url }) => {
-  const { body } = await got(url, { json: true })
-  return body
-})
+const pokeLoader = transform(2, async ({ url }) => got(url).json())
 
-// string together all our functions
+// string together all our functions with a flatten to get one pokemon at a time
 const pokePipe = pipeline(pokedex, bufferTwo, flatten, pokeLoader)
 
 // lets do it team!
