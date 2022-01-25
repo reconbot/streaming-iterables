f7d64c5b0df435167446683248af7aadc52a81d9
Richard Scarrott <riscarrott@googlemail.com>
2022-01-25 22:19:18 +0000

    Lint

--- lib/throttle-test.ts
+++ lib/throttle-test.ts
@@ -1,182 +1,182 @@
-import { assert } from "chai";
-import { throttle } from "./throttle";
-import { promiseImmediate } from "./util-test";
-import * as sinon from "sinon";
+import { assert } from 'chai'
+import { throttle } from './throttle'
+import { promiseImmediate } from './util-test'
+import * as sinon from 'sinon'
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 async function* asyncNumbers(max: number) {
-  let num = 1;
+  let num = 1
   while (num <= max) {
-    yield await promiseImmediate(num);
-    num++;
+    yield await promiseImmediate(num)
+    num++
   }
 }
 
 function* numbers(max: number) {
-  let num = 1;
+  let num = 1
   while (num <= max) {
-    yield num;
-    num++;
+    yield num
+    num++
   }
 }
 
-describe("throttle", () => {
-  let clock: ReturnType<typeof sinon.useFakeTimers>;
+describe('throttle', () => {
+  let clock: ReturnType<typeof sinon.useFakeTimers>
 
   beforeEach(() => {
-    clock = sinon.useFakeTimers();
-  });
+    clock = sinon.useFakeTimers()
+  })
 
   afterEach(() => {
-    clock.restore();
-  });
+    clock.restore()
+  })
 
-  async function* withTimestamp(iterable: any) {
+  async function* withTimestamp<T>(iterable: AsyncIterable<T>) {
     for await (const value of iterable) {
-      yield { value, timestamp: `${clock.now}ms` };
+      yield { value, timestamp: `${clock.now}ms` }
     }
   }
 
-  it("throttles sync iterators, 1 every 1s", async () => {
-    const src = withTimestamp(throttle(1, 1000, numbers(5)));
-    const promisedValues = new Promise(async (resolve) => {
-      const values: any[] = [];
+  it('throttles sync iterators, 1 every 1s', async () => {
+    const src = withTimestamp(throttle(1, 1000, numbers(5)))
+    const promisedValues = new Promise(async resolve => {
+      const vals: any[] = []
       for await (const value of src) {
-        values.push(value);
+        vals.push(value)
       }
-      resolve(values);
-    });
-    clock.runAllAsync();
-    const values = await promisedValues;
+      resolve(vals)
+    })
+    clock.runAllAsync()
+    const values = await promisedValues
     assert.deepEqual(values, [
-      { value: 1, timestamp: "0ms" },
-      { value: 2, timestamp: "1000ms" },
-      { value: 3, timestamp: "2000ms" },
-      { value: 4, timestamp: "3000ms" },
-      { value: 5, timestamp: "4000ms" },
-    ]);
-    assert.equal((await src.next()).done, true);
-  });
+      { value: 1, timestamp: '0ms' },
+      { value: 2, timestamp: '1000ms' },
+      { value: 3, timestamp: '2000ms' },
+      { value: 4, timestamp: '3000ms' },
+      { value: 5, timestamp: '4000ms' },
+    ])
+    assert.equal((await src.next()).done, true)
+  })
 
-  it("throttles async iterators, 1 every 1s", async () => {
-    const src = withTimestamp(throttle(1, 1000, asyncNumbers(5)));
-    const promisedValues = new Promise(async (resolve) => {
-      const values: any[] = [];
+  it('throttles async iterators, 1 every 1s', async () => {
+    const src = withTimestamp(throttle(1, 1000, asyncNumbers(5)))
+    const promisedValues = new Promise(async resolve => {
+      const vals: any[] = []
       for await (const value of src) {
-        values.push(value);
+        vals.push(value)
       }
-      resolve(values);
-    });
-    clock.runAllAsync();
-    const values = await promisedValues;
+      resolve(vals)
+    })
+    clock.runAllAsync()
+    const values = await promisedValues
     assert.deepEqual(values, [
-      { value: 1, timestamp: "0ms" },
-      { value: 2, timestamp: "1000ms" },
-      { value: 3, timestamp: "2000ms" },
-      { value: 4, timestamp: "3000ms" },
-      { value: 5, timestamp: "4000ms" },
-    ]);
-    assert.equal((await src.next()).done, true);
-  });
+      { value: 1, timestamp: '0ms' },
+      { value: 2, timestamp: '1000ms' },
+      { value: 3, timestamp: '2000ms' },
+      { value: 4, timestamp: '3000ms' },
+      { value: 5, timestamp: '4000ms' },
+    ])
+    assert.equal((await src.next()).done, true)
+  })
 
-  it("throttles async iterators, 2 every 1s", async () => {
-    const src = withTimestamp(throttle(2, 1000, asyncNumbers(5)));
-    const promisedValues = new Promise(async (resolve) => {
-      const values: any[] = [];
+  it('throttles async iterators, 2 every 1s', async () => {
+    const src = withTimestamp(throttle(2, 1000, asyncNumbers(5)))
+    const promisedValues = new Promise(async resolve => {
+      const vals: any[] = []
       for await (const value of src) {
-        values.push(value);
+        vals.push(value)
       }
-      resolve(values);
-    });
-    clock.runAllAsync();
-    const values = await promisedValues;
+      resolve(vals)
+    })
+    clock.runAllAsync()
+    const values = await promisedValues
     assert.deepEqual(values, [
-      { value: 1, timestamp: "0ms" },
-      { value: 2, timestamp: "0ms" },
-      { value: 3, timestamp: "1000ms" },
-      { value: 4, timestamp: "1000ms" },
-      { value: 5, timestamp: "2000ms" },
-    ]);
-    assert.equal((await src.next()).done, true);
-  });
+      { value: 1, timestamp: '0ms' },
+      { value: 2, timestamp: '0ms' },
+      { value: 3, timestamp: '1000ms' },
+      { value: 4, timestamp: '1000ms' },
+      { value: 5, timestamp: '2000ms' },
+    ])
+    assert.equal((await src.next()).done, true)
+  })
 
-  it("throttles async iterators, 4 every 6s", async () => {
-    const src = withTimestamp(throttle(4, 6000, asyncNumbers(9)));
-    const promisedValues = new Promise(async (resolve) => {
-      const values: any[] = [];
+  it('throttles async iterators, 4 every 6s', async () => {
+    const src = withTimestamp(throttle(4, 6000, asyncNumbers(9)))
+    const promisedValues = new Promise(async resolve => {
+      const vals: any[] = []
       for await (const value of src) {
-        values.push(value);
+        vals.push(value)
       }
-      resolve(values);
-    });
-    clock.runAllAsync();
-    const values = await promisedValues;
+      resolve(vals)
+    })
+    clock.runAllAsync()
+    const values = await promisedValues
     assert.deepEqual(values, [
-      { value: 1, timestamp: "0ms" },
-      { value: 2, timestamp: "0ms" },
-      { value: 3, timestamp: "0ms" },
-      { value: 4, timestamp: "0ms" },
-      { value: 5, timestamp: "6000ms" },
-      { value: 6, timestamp: "6000ms" },
-      { value: 7, timestamp: "6000ms" },
-      { value: 8, timestamp: "6000ms" },
-      { value: 9, timestamp: "12000ms" },
-    ]);
-  });
+      { value: 1, timestamp: '0ms' },
+      { value: 2, timestamp: '0ms' },
+      { value: 3, timestamp: '0ms' },
+      { value: 4, timestamp: '0ms' },
+      { value: 5, timestamp: '6000ms' },
+      { value: 6, timestamp: '6000ms' },
+      { value: 7, timestamp: '6000ms' },
+      { value: 8, timestamp: '6000ms' },
+      { value: 9, timestamp: '12000ms' },
+    ])
+  })
 
-  it("avoids over-throttling if the consumer is slower than the throttling config", async () => {
-    const src = withTimestamp(throttle(1, 500, asyncNumbers(7)));
-    const promisedValues = new Promise(async (resolve) => {
-      const values: any[] = [];
-      values.push((await src.next()).value); // 1
-      await sleep(1000);
-      values.push((await src.next()).value); // 2
-      await sleep(250);
-      values.push((await src.next()).value); // 3
-      await sleep(3000);
-      values.push((await src.next()).value); // 4
-      await sleep(100);
-      values.push((await src.next()).value); // 5
-      await sleep(1000);
-      values.push((await src.next()).value); // 6
-      await sleep(60000);
-      values.push((await src.next()).value); // 7
-      resolve(values);
-    });
-    clock.runAllAsync();
-    const values = await promisedValues;
+  it('avoids over-throttling if the consumer is slower than the throttling config', async () => {
+    const src = withTimestamp(throttle(1, 500, asyncNumbers(7)))
+    const promisedValues = new Promise(async resolve => {
+      const vals: any[] = []
+      vals.push((await src.next()).value) // 1
+      await sleep(1000)
+      vals.push((await src.next()).value) // 2
+      await sleep(250)
+      vals.push((await src.next()).value) // 3
+      await sleep(3000)
+      vals.push((await src.next()).value) // 4
+      await sleep(100)
+      vals.push((await src.next()).value) // 5
+      await sleep(1000)
+      vals.push((await src.next()).value) // 6
+      await sleep(60000)
+      vals.push((await src.next()).value) // 7
+      resolve(vals)
+    })
+    clock.runAllAsync()
+    const values = await promisedValues
     assert.deepEqual(values, [
-      { value: 1, timestamp: "0ms" },
-      { value: 2, timestamp: "1000ms" },
-      { value: 3, timestamp: "1500ms" }, // throttled
-      { value: 4, timestamp: "4500ms" },
-      { value: 5, timestamp: "5000ms" }, // throttled
-      { value: 6, timestamp: "6000ms" },
-      { value: 7, timestamp: "66000ms" },
-    ]);
-    assert.equal((await src.next()).done, true);
-  });
+      { value: 1, timestamp: '0ms' },
+      { value: 2, timestamp: '1000ms' },
+      { value: 3, timestamp: '1500ms' }, // throttled
+      { value: 4, timestamp: '4500ms' },
+      { value: 5, timestamp: '5000ms' }, // throttled
+      { value: 6, timestamp: '6000ms' },
+      { value: 7, timestamp: '66000ms' },
+    ])
+    assert.equal((await src.next()).done, true)
+  })
 
-  it("is curryable", async () => {
-    const throttle3PerSecond = throttle(3, 1000);
-    const src = withTimestamp(throttle3PerSecond(asyncNumbers(5)));
-    const promisedValues = new Promise(async (resolve) => {
-      const values: any[] = [];
+  it('is curryable', async () => {
+    const throttle3PerSecond = throttle(3, 1000)
+    const src = withTimestamp(throttle3PerSecond(asyncNumbers(5)))
+    const promisedValues = new Promise(async resolve => {
+      const vals: any[] = []
       for await (const value of src) {
-        values.push(value);
+        vals.push(value)
       }
-      resolve(values);
-    });
-    clock.runAllAsync();
-    const values = await promisedValues;
+      resolve(vals)
+    })
+    clock.runAllAsync()
+    const values = await promisedValues
     assert.deepEqual(values, [
-      { value: 1, timestamp: "0ms" },
-      { value: 2, timestamp: "0ms" },
-      { value: 3, timestamp: "0ms" },
-      { value: 4, timestamp: "1000ms" },
-      { value: 5, timestamp: "1000ms" },
-    ]);
-    assert.equal((await src.next()).done, true);
-  });
-});
+      { value: 1, timestamp: '0ms' },
+      { value: 2, timestamp: '0ms' },
+      { value: 3, timestamp: '0ms' },
+      { value: 4, timestamp: '1000ms' },
+      { value: 5, timestamp: '1000ms' },
+    ])
+    assert.equal((await src.next()).done, true)
+  })
+})
--- lib/throttle.ts
+++ lib/throttle.ts
@@ -1,56 +1,40 @@
 /// <reference lib="esnext.asynciterable" />
-import { AnyIterable } from "./types";
+import { AnyIterable } from './types'
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 // TODO: Rewrite as the `time = Date.now()` is repetitive / clumsy, maybe a `while`
 // loop will make it simpler + should be able to call `next` earlier, while sleeping?
-async function* _throttle<T>(
-  num: number,
-  ms: number,
-  iterable: AnyIterable<T>
-) {
-  let sent = 0;
-  let time: number | undefined;
+async function* _throttle<T>(num: number, ms: number, iterable: AnyIterable<T>) {
+  let sent = 0
+  let time: number | undefined
   for await (const val of iterable) {
     if (sent < num) {
-      if (typeof time === "undefined") {
-        time = Date.now();
+      if (typeof time === 'undefined') {
+        time = Date.now()
       }
-      sent++;
-      yield val;
-      continue;
+      sent++
+      yield val
+      continue
     }
     // Only wait if the window hasn't already passed while we were
     // yielding the previous values.
-    const elapsedMs = Date.now() - time!;
-    const waitFor = ms - elapsedMs;
+    const elapsedMs = Date.now() - time!
+    const waitFor = ms - elapsedMs
     if (waitFor > 0) {
-      await sleep(waitFor);
+      await sleep(waitFor)
     }
-    time = Date.now();
-    sent = 1;
-    yield val;
+    time = Date.now()
+    sent = 1
+    yield val
   }
 }
 
-export function throttle<T>(
-  num: number,
-  ms: number
-): (iterable: AnyIterable<T>) => AsyncGenerator<T>;
-export function throttle<T>(
-  num: number,
-  ms: number,
-  iterable: AnyIterable<T>
-): AsyncGenerator<T>;
-export function throttle<T>(
-  num: number,
-  ms: number,
-  iterable?: AnyIterable<T>
-) {
+export function throttle<T>(num: number, ms: number): (iterable: AnyIterable<T>) => AsyncGenerator<T>
+export function throttle<T>(num: number, ms: number, iterable: AnyIterable<T>): AsyncGenerator<T>
+export function throttle<T>(num: number, ms: number, iterable?: AnyIterable<T>) {
   if (iterable === undefined) {
-    return (curriedIterable: AnyIterable<T>) =>
-      _throttle(num, ms, curriedIterable);
+    return (curriedIterable: AnyIterable<T>) => _throttle(num, ms, curriedIterable)
   }
-  return _throttle(num, ms, iterable);
+  return _throttle(num, ms, iterable)
 }
