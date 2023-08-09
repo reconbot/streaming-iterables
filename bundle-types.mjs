ca5bdd8dfc250885716ef00af122e333b00035e7
Francis Gulotta <francis.gulotta@vercel.com>
2023-08-09 18:02:31 -0400

    No need

--- bundle-types.mjs
+++ bundle-types.mjs
@@ -4,7 +4,6 @@ await createBundle({
   project: 'tsconfig.json',
   output: 'dist/index.d.ts',
   modules: {
-    // 'streaming-iterables': './dist-ts/index.js',
     'streaming-iterables': './lib/index.ts',
   }
 })
