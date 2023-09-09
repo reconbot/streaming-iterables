56f92a1a2df758d6869df204081bfaca5b3aee07
Francis Gulotta <wizard@roborooter.com>
2023-09-08 20:35:37 -0400

    fix: types for esmodules
    had to roll back dts-buddy and opened https://github.com/Rich-Harris/dts-buddy/issues/55 about it

--- bundle-types.mjs
+++ bundle-types.mjs
@@ -7,3 +7,11 @@ await createBundle({
     'streaming-iterables': './lib/index.ts',
   }
 })
+
+await createBundle({
+  project: 'tsconfig.json',
+  output: 'dist/index.d.mts',
+  modules: {
+    'streaming-iterables': './lib/index.ts',
+  }
+})
